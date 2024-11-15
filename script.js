18a65465b11ee7ef1468b2019471c70870f5d0d3
KHANHTRANG-22660721 <khanhtrang22.7@gmail.com>
2024-11-15 12:07:55 +0700

    upload

---
+++ script.js
@@ -0,0 +1,10 @@
+/* script.js */
+function showLogin() {
+    document.getElementById('loginForm').classList.remove('hidden');
+    document.getElementById('registerForm').classList.add('hidden');
+}
+
+function showRegister() {
+    document.getElementById('loginForm').classList.add('hidden');
+    document.getElementById('registerForm').classList.remove('hidden');
+}
